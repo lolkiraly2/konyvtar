d15b2efc9c93716ea86235ee2ab037baa4030ff9
lolkiraly2 <szebigeri@gmail.com>
2024-05-13 18:04:46 +0200

    Breeze telepítés

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Models\Book;
 use App\Models\Rent;
 use Illuminate\Support\Facades\Route;
@@ -19,8 +20,6 @@ use App\Http\Controllers\RenthistoryController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
 Route::resource('people', PersonController::class);
 
 Route::resource('isbns', IsbnController::class);
@@ -39,7 +38,18 @@ Route::get('get-person-data', [PersonController::class,'getPersonData'])->name('
 Route::get('get-book-data', [BookController::class,'getBookData'])->name('get.bookdata');
 Route::get('get-isbn-data', [IsbnController::class,'getisbn'])->name('get.isbn');
 
-
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
